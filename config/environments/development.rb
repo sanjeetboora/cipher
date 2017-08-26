58b470b3ca2af840fc7dcb87a0c4573676f6a6be
sanjeet <you@example.com>
2017-08-26 16:25:11 +0530

    added questions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -35,7 +36,15 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'helpermail41@gmail.com',
+  password:             'sanketpglet',
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
